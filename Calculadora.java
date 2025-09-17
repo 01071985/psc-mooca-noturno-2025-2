047a9b165f22f81f326f239af328a9ed827a09a8
Oscar Joze dos Santos <825224748@ulife.com.br>
2025-09-16 21:39:42 -0300

    Criando a calculadora aula 1

---
+++ Calculadora.java
@@ -0,0 +1,30 @@
+public class Calculadora {
+
+    public int soma(int a, int b) {
+        int resultado;
+        resultado = a + b;
+        return resultado;
+
+    }
+
+    public int subtracao(int a, int b) {
+        int resultado;
+        resultado = a - b;
+        return resultado;
+
+    }
+
+    public int multiplicacao(int a, int b) {
+        int resultado;
+        resultado = a * b;
+        return resultado;
+
+    }
+
+    public int divisao(int a, int b) {
+        int resultado;
+        resultado = a / b;
+        return resultado;
+
+    }
+}
